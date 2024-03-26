81f5497b33e62a14e11a8d4d00863390b37ac2c5
Harrison Palmer <hscopalm@gmail.com>
2024-03-25 21:12:22 -0700

    Adding image conversion and zoom process killing

--- background_changer.py
+++ background_changer.py
@@ -1,7 +1,10 @@
 import os, platform, shutil
-from urllib import request
+import requests
+import json
 import json
 import datetime as dt
+from PIL import Image
+import psutil
 
 
 def read_config(filename):
@@ -65,17 +68,10 @@ stage = "v1"
 
 # first, we need to get the available holidays from the API for today
 holidays_endpoint = "{root}/{stage}/holidays".format(root=root, stage=stage)
-holidays_req = request.Request(
-    holidays_endpoint,
-    headers=header,
-    method="POST",
-)
 
-# take the response from the API and print the holidays with their index
-with request.urlopen(holidays_req) as resp:
-    holidays = dict(
-        zip(range(0, 1000), resp.read().decode("utf-8").replace('"', "").split("\\n"))
-    )
+with requests.post(holidays_endpoint, headers=header) as resp:
+
+    holidays = dict(zip(range(0, 1000), resp.text.replace('"', "").split("\\n")))
 
 # filter out the birthdays if the user doesn't want them
 if not include_birthdays:
@@ -87,6 +83,7 @@ if not include_birthdays:
 else:
     holidays_filt = holidays
 
+# take the response from the API and print the holidays with their index
 print("Available holidays for the provided date: ")
 for index in holidays_filt:
     print("[{index}] {holiday}".format(index=index, holiday=holidays[index]))
@@ -108,38 +105,57 @@ print("-" * 50)
 print("Getting the image for the chosen holiday...")
 
 # we need to send the choice to the API to get the image
-data = str(json.dumps({"holiday_name": choice_name})).encode("utf-8")
-
 img_endpoint = "{root}/{stage}/images".format(root=root, stage=stage)
-img_req = request.Request(
-    img_endpoint,
-    headers=header,
-    method="POST",
-    data=data,
-)
 
 # take the response from the API and download the image
-with request.urlopen(img_req) as resp:
-    img_url = resp.read().decode("utf-8").replace('"', "")
+with requests.post(
+    img_endpoint, headers=header, json={"holiday_name": choice_name}
+) as resp:
+    img_url = resp.text.replace('"', "")
 
-print("Downloading the image at: " + img_url)
-
-# download the image
-img_download_req = request.Request(
-    img_url,
-    headers=header,
-)
 
 # take the response from the API and download the image
 os.makedirs("img", exist_ok=True)  # create the directory if it doesn't exist
 
-with request.urlopen(img_download_req) as resp:
+# clear the directory of any files from prior runs
+file_list = os.listdir("img")
+for file_name in file_list:
+    file_path = os.path.join("img", file_name)
+    os.remove(file_path)
 
-    with open("img/background.jpg", "wb") as f:
-        f.write(resp.read())
+with requests.get(img_url, headers=header, allow_redirects=True, stream=True) as resp:
+
+    redirected_img_url = resp.url
+    print("Downloading the image at: " + redirected_img_url)
+
+    # pull the file extension from the url
+    orig_file_ext = os.path.splitext(redirected_img_url)[1]
+    print("File extension: ", orig_file_ext)
+
+    output_file = "img/background.jpg"
+    with open(output_file, "wb") as f:
+
+        if orig_file_ext == ".webp":
+            print("WebP file detected, converting the image to the correct format...")
+
+            im = Image.open(resp.raw).convert("RGB")
+            im.save(f, "jpeg")
+        else:
+            shutil.copyfileobj(resp.raw, f)
 
 print("Image downloaded successfully!")
 print("\n")
+
+# check if zoom is currently running and kill it if so
+for proc in psutil.process_iter():
+    # check whether the process name matches
+    if proc.name() == "Zoom.exe":
+        print("-" * 50)
+        print("Zoom is currently running, closing the application...")
+        proc.kill()
+        print("Zoom closed successfully!")
+        print("\n")
+
 
 print("-" * 50)
 print("Setting the image as your Zoom background...")
@@ -169,7 +185,7 @@ print("...")
 
 # %% copy chosen image over provided background (and thumbnail for now)
 # just replacing both with the same image, no perfect method for determining thumb vs back
-shutil.copy("img/background.jpg", img_pair["background"])
-shutil.copy("img/background.jpg", img_pair["thumbnail"])
+shutil.copy(output_file, img_pair["background"])
+shutil.copy(output_file, img_pair["thumbnail"])
 
 print("Background image replaced successfully!")
